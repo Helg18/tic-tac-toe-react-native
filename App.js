6bec2e98b13b92341f06b5a2fa22c7e5659eeddd
Henry Leon Gomez <helg18@gmail.com>
2019-05-30 11:49:28 -0400

    Drawing tic tac toe board

--- App.js
+++ App.js
@@ -1,21 +1,32 @@
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import {StyleSheet, Text, View, TouchableOpacity} from 'react-native';
 
 type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        {/* Fila */}
+        <View style={styles.row}>
+          <View style={[styles.casilla, { borderTopWidth: 0, borderLeftWidth: 0 } ]}></View>
+          <View style={[styles.casilla, { borderTopWidth: 0 }]}></View>
+          <View style={[styles.casilla, { borderTopWidth: 0, borderRightWidth: 0}]}></View>
+        </View>
+
+        {/* Fila */}
+        <View style={styles.row}>
+          <View style={[styles.casilla, { borderLeftWidth: 0}]}></View>
+          <View style={[styles.casilla, {}]}></View>
+          <View style={[styles.casilla, { borderRightWidth: 0 }]}></View>
+        </View>
+
+        {/* Fila */}
+        <View style={styles.row}>
+          <View style={[styles.casilla, { borderBottomWidth: 0, borderLeftWidth: 0}]}></View>
+          <View style={[styles.casilla, { borderBottomWidth: 0 }]}></View>
+          <View style={[styles.casilla, { borderBottomWidth: 0, borderRightWidth: 0}]}></View>
+        </View>
+
       </View>
     );
   }
@@ -28,14 +39,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  casilla: {
+    borderWidth: 3,
+    height: 100,
+    width: 100
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  row: {
+    flexDirection: "row"
+  }
 });
